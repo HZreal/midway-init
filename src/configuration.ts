78b4e68a969a083f1af1e08da27a1ee52f831977
HZreal <huangzhen_happy@163.com>
2023-04-06 09:48:46 +0800

    'enable swagger'

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,29 +3,35 @@ import * as koa from '@midwayjs/koa';
 import * as validate from '@midwayjs/validate';
 import * as info from '@midwayjs/info';
 import { join } from 'path';
+import * as swagger from '@midwayjs/swagger';
 // import { DefaultErrorFilter } from './filter/default.filter';
 // import { NotFoundFilter } from './filter/notfound.filter';
 import { ReportMiddleware } from './middleware/report.middleware';
 
 @Configuration({
-  imports: [
-    koa,
-    validate,
-    {
-      component: info,
-      enabledEnvironment: ['local'],
-    },
-  ],
-  importConfigs: [join(__dirname, './config')],
+    imports: [
+        koa,
+        validate,
+        {
+            component: info,
+            enabledEnvironment: ['local'],
+        },
+        {
+            component: swagger,
+            // enabledEnvironment: ['local'],
+        },
+    ],
+    importConfigs: [join(__dirname, './config')],
 })
 export class ContainerLifeCycle {
-  @App()
-  app: koa.Application;
+    @App()
+    app: koa.Application;
 
-  async onReady() {
-    // add middleware
-    this.app.useMiddleware([ReportMiddleware]);
-    // add filter
-    // this.app.useFilter([NotFoundFilter, DefaultErrorFilter]);
-  }
+    async onReady() {
+        // add middleware
+        this.app.useMiddleware([ReportMiddleware]);
+
+        // add filter
+        // this.app.useFilter([NotFoundFilter, DefaultErrorFilter]);
+    }
 }
