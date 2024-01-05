06b75184dec69b2f71c6510aa957bad94fe509d3
HZreal <huangzhen_happy@163.com>
2024-01-05 15:24:08 +0800

    refactor: adjust the field of Entity

--- src/common/baseModel.ts
+++ src/common/baseModel.ts
@@ -3,33 +3,47 @@ import {
     CreateDateColumn,
     DeleteDateColumn,
     PrimaryGeneratedColumn,
+    UpdateDateColumn,
 } from 'typeorm';
-import { ApiProperty } from '@midwayjs/swagger';
 
 /*
 模型基类
  */
 export class BaseEntity {
-    @ApiProperty({ description: 'id' })
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn({ name: 'id', type: 'integer', comment: '主键id' })
     id: number;
 
     // 创建时间
     @CreateDateColumn({
         name: 'create_time',
+        type: 'timestamp',
+        default: () => 'NOW()',
+        comment: '创建时间',
     })
     createTime: Date;
+
+    // 更新时间
+    @UpdateDateColumn({
+        name: 'update_time',
+        type: 'timestamp',
+        default: () => 'NOW()',
+        comment: '更新时间',
+    })
+    updateTime: Date;
 
     // 删除时间
     @DeleteDateColumn({
         name: 'delete_time',
-        select: false,
+        type: 'timestamp',
+        default: null,
+        comment: '删除时间',
     })
     deleteTime: Date;
 
     // 是否逻辑删除
     @Column({
         name: 'is_delete',
+        type: 'boolean',
         default: false,
         select: false,
     })
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -2,17 +2,64 @@
  * @author sizhong
  * @date 2023-05-26
  */
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column } from 'typeorm';
 import { Entity } from 'typeorm';
+import { ApiProperty } from '@midwayjs/swagger';
+import { BaseEntity } from '../common/baseModel';
 
 @Entity('tb_user', { schema: 'public' })
-export class UserEntity {
-    @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
-    id?: number;
+export class UserEntity extends BaseEntity {
+    // 用户名
+    @ApiProperty({ description: '用户名' })
+    @Column({
+        name: 'username',
+        type: 'varchar',
+        length: 32,
+        nullable: false,
+        comment: '用户名',
+    })
+    username: string;
 
-    @Column('character varying', { name: 'name', nullable: true, length: 32 })
-    name: string | null;
+    // 密码
+    @Column({
+        name: 'password',
+        type: 'varchar',
+        length: 64,
+        nullable: false,
+        comment: '密码',
+        // select: false, // 查询时不会返回此字段
+    })
+    password: string;
 
-    @Column('text', { name: 'desc', nullable: true })
-    desc: string | null;
+    // 邮箱
+    @ApiProperty({ description: '邮箱' })
+    @Column({
+        name: 'email',
+        type: 'varchar',
+        length: 64,
+        nullable: true,
+        comment: '邮箱',
+    })
+    email: string;
+
+    // 状态
+    @ApiProperty({ description: '状态' })
+    @Column({
+        name: 'status',
+        type: 'boolean',
+        default: true,
+        enum: [true, false],
+        comment: '状态',
+    })
+    status: boolean;
+
+    // 是否是超级管理员
+    @Column({
+        name: 'is_super',
+        type: 'boolean',
+        default: false,
+        enum: [true, false],
+        comment: '是否是超级管理员',
+    })
+    isSuper: boolean;
 }
